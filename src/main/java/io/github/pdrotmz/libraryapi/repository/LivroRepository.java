bf5cb490eb035cca74252287a264d06e48e9b5bf
pdrotmz <pedrotomaz0210@gmail.com>
2025-01-01 20:16:36 -0300

    :sparkles: feat: adicionando lista de livros por autor

--- src/main/java/io/github/pdrotmz/libraryapi/repository/LivroRepository.java
+++ src/main/java/io/github/pdrotmz/libraryapi/repository/LivroRepository.java
@@ -1,9 +1,15 @@
 package io.github.pdrotmz.libraryapi.repository;
 
+import io.github.pdrotmz.libraryapi.model.Autor;
 import io.github.pdrotmz.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface LivroRepository extends JpaRepository<Livro, UUID> {
+
+    // Query Method
+    // select * from livro where id_autor = id
+    List<Livro> findByAutor(Autor autor);
 }
