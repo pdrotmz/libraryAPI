00daf91e58c61967677be3603178c0b85cb2855c
pdrotmz <pedrotomaz0210@gmail.com>
2025-01-01 20:12:04 -0300

    :sparkles: feat: adicionando ToString livros e fetch LAZY

--- src/main/java/io/github/pdrotmz/libraryapi/model/Autor.java
+++ src/main/java/io/github/pdrotmz/libraryapi/model/Autor.java
@@ -2,6 +2,7 @@ package io.github.pdrotmz.libraryapi.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "autor", schema = "public")
 @Data
+@ToString(exclude = {"livros"})
 public class Autor {
 
     @Id
@@ -26,7 +28,6 @@ public class Autor {
     @Column(name = "nacionalidade", length = 50, nullable = false)
     private String nacionalidade;
 
-    // @OneToMany(mappedBy = "autor")
-    @Transient // Não considera o livros como coluna
+    @OneToMany(mappedBy = "autor", fetch = FetchType.LAZY)
     private List<Livro> livros;
 }
